bb1c938cce2bcee68327a2e042f89baa1ecaeb36
Pradeep <pradeep99909@gmail.com>
2020-06-21 22:07:46 +0530

    add person in Tree

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const data = require("./data.json");
+const readline = require("readline");
+const { fstat } = require("fs");
+class Family_Tree {
+  constructor() {
+    this.tree = {};
+  }
+
+  add_person(name_of_person) {
+    const fs = require("fs");
+
+    fs.readFile("data.json", (err, data) => {
+      if (err) {
+        return console.error(err);
+      }
+      var data = JSON.parse(data);
+      var length = Object.keys(data).length;
+      data[length + 1] = {
+        name: name_of_person,
+      };
+      fs.writeFileSync("data.json", JSON.stringify(data));
+    });
+  }
+}
+
+module.exports = Family_Tree;
