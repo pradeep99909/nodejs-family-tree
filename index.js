3daf511d8db7430aa63cd4ea767b736047f2976b
Pradeep <pradeep99909@gmail.com>
2020-06-21 22:18:30 +0530

    add relationship

--- index.js
+++ index.js
@@ -22,6 +22,26 @@ class Family_Tree {
       fs.writeFileSync("data.json", JSON.stringify(data));
     });
   }
+
+  add_relationship(relation) {
+    const fs = require("fs");
+
+    fs.readFile("data.json", (err, data) => {
+      if (err) {
+        return console.error(err);
+      }
+      var data = JSON.parse(data);
+      var length = Object.keys(data).length;
+      var name = Object.keys(data[length]);
+
+      data[length] = {
+        name: data[length][name],
+        relationship: relation,
+      };
+      console.log(data);
+      fs.writeFileSync("data.json", JSON.stringify(data));
+    });
+  }
 }
 
 module.exports = Family_Tree;
