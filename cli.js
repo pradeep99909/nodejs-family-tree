c066986f344378eec86c91ccb82a62c77a8cdd8b
Pradeep <pradeep99909@gmail.com>
2020-06-22 23:18:20 +0530

    add person add relationship connect function cli

--- cli.js
+++ cli.js
@@ -6,16 +6,20 @@ var Tree = new Family_Tree();
 var argv = process.argv.slice(2);
 switch (argv[0]) {
   case "add-person":
-    console.log("add person");
+    Tree.add_person(argv[1]);
     break;
   case "add-relationship":
-    console.log("add relationship");
+    Tree.add_relationship(argv[1]);
     break;
   case "connect":
-    console.log("connect");
-    break;
-  case "count":
-    console.log("count");
+    if (argv.length !== 6) {
+      console.log("Wrong command");
+      console.log(
+        "Is may be like family-tree connect <name 1> as <relationship> of <name 2>"
+      );
+    } else {
+      Tree.connect(argv[1], argv[5], argv[3]);
+    }
     break;
   default:
     console.log("Entered wrong command");
